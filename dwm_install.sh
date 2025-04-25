f6b467cf4ff79ae80203d69dceca399a7a476ac9
ChrLos <lindsey.elizabeta.a4mzm@slmail.me>
2025-04-25 04:34:12 -0400

    Prefer Nala

--- dwm_install.sh
+++ dwm_install.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-sudo apt install xorg dwm xdm thunar alacritty pulseaudio alsamixergui lynx j4-dmenu-desktop
+sudo apt install nala -y
+sudo nala install xorg dwm xdm thunar alacritty pulseaudio alsamixergui lynx j4-dmenu-desktop -y
