efb80efaf47f83267b34f8f0c3511e4bf47d23f2
ChrLos <lindsey.elizabeta.a4mzm@slmail.me>
2025-04-25 05:37:52 -0400

    fix: Use Admin to Apply Patches

--- dwm_mod_install.sh
+++ dwm_mod_install.sh
@@ -7,8 +7,8 @@ sudo nala install git make gcc libx11-dev libxft-dev libxinerama-dev -y
 git clone https://github.com/bakkeby/dwm-flexipatch.git ~/dwm-flexipatch
 
 # Apply Patches
-curl -fsSL https://raw.githubusercontent.com/ChrLos/my_scripts/refs/heads/main/patches/patches.h > ~/dwm-flexipatch/patches.h
-curl -fsSL https://raw.githubusercontent.com/ChrLos/my_scripts/refs/heads/main/patches/config.h > ~/dwm-flexipatch/config.h
+curl -fsS https://raw.githubusercontent.com/ChrLos/my_scripts/refs/heads/main/patches/patches.h | sudo tee ~/dwm-flexipatch/patches.h > /dev/null
+curl -fsS https://raw.githubusercontent.com/ChrLos/my_scripts/refs/heads/main/patches/config.h | sudo tee ~/dwm-flexipatch/config.h > /dev/null
 
 # Add dwm session to xdm
 echo "exec dwm" > ~/.xsession
