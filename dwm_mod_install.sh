13a242932dbe6260cd6b88914cac3f8fd805a6e2
ChrLos <lindsey.elizabeta.a4mzm@slmail.me>
2025-04-25 05:23:44 -0400

    Add dwm-flexipatch

---
+++ dwm_mod_install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+sudo apt install nala -y
+sudo nala install xorg dmenu xdm thunar alacritty pulseaudio alsamixergui lynx j4-dmenu-desktop -y
+
+# Required Packages to Compile dwm-flexipatch
+sudo nala install git make gcc libx11-dev libxft-dev libxinerama-dev -y
+git clone https://github.com/bakkeby/dwm-flexipatch.git ~/dwm-flexipatch
+
+# Copy the Patches
+cp ./patches/patches.h ~/dwm-flexipatch/patches.h
+cp ./patches/config.h ~/dwm-flexipatch/config.h
+
+# Add dwm session to xdm
+echo "exec dwm" > ~/.xsession
+
+# Start Compiling
+cd ~/dwm-flexipatch
+sudo make clean install
