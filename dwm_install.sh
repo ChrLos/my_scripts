ee4933ec5ff8c4a35b031d527b054b0234c60b8d
ChrLos <lindsey.elizabeta.a4mzm@slmail.me>
2025-04-29 05:20:43 -0400

    Set Master Volume to 100%

--- dwm_install.sh
+++ dwm_install.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 sudo apt install nala -y
 sudo nala install xorg dwm xdm thunar alacritty pulseaudio alsamixergui lynx j4-dmenu-desktop -y
+
+# Set the Master volume to 100%
+amixer set Master 100%
