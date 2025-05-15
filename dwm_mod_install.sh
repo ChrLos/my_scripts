3395f189115034a0844613eb0780ca6dd1762ded
ChrLos <lindsey.elizabeta.a4mzm@slmail.me>
2025-05-15 05:11:44 -0400

    Variable for Path, Change to /tmp/ Instead

--- dwm_mod_install.sh
+++ dwm_mod_install.sh
@@ -3,12 +3,14 @@ sudo apt install nala -y
 sudo nala install xorg dmenu xdm thunar alacritty pulseaudio alsamixergui lynx j4-dmenu-desktop -y
 
 # Required Packages to Compile dwm-flexipatch
+dwmflexipatch_path="/tmp/dwm-flexipatch"
+
 sudo nala install git make gcc libx11-dev libxft-dev libxinerama-dev -y
-git clone https://github.com/bakkeby/dwm-flexipatch.git ~/dwm-flexipatch
+git clone https://github.com/bakkeby/dwm-flexipatch.git $dwmflexipatch_path
 
 # Apply Patches
-curl -fsS https://raw.githubusercontent.com/ChrLos/my_scripts/refs/heads/main/patches/patches.h | sudo tee ~/dwm-flexipatch/patches.h > /dev/null
-curl -fsS https://raw.githubusercontent.com/ChrLos/my_scripts/refs/heads/main/patches/config.h | sudo tee ~/dwm-flexipatch/config.h > /dev/null
+curl -fsS https://raw.githubusercontent.com/ChrLos/my_scripts/refs/heads/main/patches/patches.h | sudo tee $dwmflexipatch_path/patches.h > /dev/null
+curl -fsS https://raw.githubusercontent.com/ChrLos/my_scripts/refs/heads/main/patches/config.h | sudo tee $dwmflexipatch_path/config.h > /dev/null
 
 # Add dwm session to xdm
 echo "exec dwm" > ~/.xsession
