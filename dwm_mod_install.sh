40f79a166b6aecb7884e38fa561c977bf331dc3c
ChrLos <lindsey.elizabeta.a4mzm@slmail.me>
2025-05-16 05:01:00 -0400

    Moving Variable to Top Line, Use Variable for Url

--- dwm_mod_install.sh
+++ dwm_mod_install.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
+dwmflexipatch_path="/tmp/dwm-flexipatch"
+patches_url="https://raw.githubusercontent.com/ChrLos/dwm-scripts/refs/heads/main/patches"
+
 sudo apt install nala -y
 sudo nala install xorg dmenu xdm thunar alacritty pulseaudio alsamixergui lynx j4-dmenu-desktop -y
 
 # Required Packages to Compile dwm-flexipatch
-dwmflexipatch_path="/tmp/dwm-flexipatch"
-
 sudo nala install git make gcc libx11-dev libxft-dev libxinerama-dev -y
 git clone https://github.com/bakkeby/dwm-flexipatch.git $dwmflexipatch_path
 
 # Apply Patches
-curl -fsS https://raw.githubusercontent.com/ChrLos/my_scripts/refs/heads/main/patches/patches.h | sudo tee $dwmflexipatch_path/patches.h > /dev/null
-curl -fsS https://raw.githubusercontent.com/ChrLos/my_scripts/refs/heads/main/patches/config.h | sudo tee $dwmflexipatch_path/config.h > /dev/null
+curl -fsSL $patches_url/patches.h | sudo tee $dwmflexipatch_path/patches.h > /dev/null
+curl -fsSL $patches_url/config.h | sudo tee $dwmflexipatch_path/config.h > /dev/null
 
 # Add dwm session to xdm
 echo "exec dwm" > ~/.xsession
