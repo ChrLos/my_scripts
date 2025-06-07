9ee5c8026530aa3a85075947d57c008005243ac9
ChrLos <lindsey.elizabeta.a4mzm@slmail.me>
2025-06-07 02:36:46 -0400

    Add Set Resolution at Startup

---
+++ setboot_resolution.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if [ $(grep -cE "xrandr --output .* --mode .*" ~/.xsession || grep -cE "xrandr --output .* --mode .*" ~/.xinitrc) -gt 0 ]; then
+    echo "Settings already exist"
+    exit 1
+fi
+
+read -p "Type Your Display Name: " display_name
+read -p "Type Your Desired Resolution: " resolution
+
+resolution_command="xrandr --output $display_name --mode $resolution"
+
+sed -i "1i$resolution_command" ~/.xsession
+sed -i "1i$resolution_command" ~/.xinitrc #Fallback incase .xsession doesn't work
