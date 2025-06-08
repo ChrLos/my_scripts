3717790aa314b9bdd64745163f280a8d8525cf9a
ChrLos <lindsey.elizabeta.a4mzm@slmail.me>
2025-06-08 09:25:08 -0400

    fix: Wrong Variable Name, Volume Script not Working

--- dwm_scripts/dwm_mod_install.sh
+++ dwm_scripts/dwm_mod_install.sh
@@ -6,13 +6,15 @@ patches_location="./patches"
 sudo apt install nala -y
 sudo nala install xorg dmenu xdm thunar alacritty pulseaudio alsamixergui lynx j4-dmenu-desktop -y
 
+./set_max_volume.sh
+
 # Required Packages to Compile dwm-flexipatch
 sudo nala install git make gcc libx11-dev libxft-dev libxinerama-dev -y
 git clone https://github.com/bakkeby/dwm-flexipatch.git $dwmflexipatch_path
 
 # Apply Patches
-sudo cp $patches_url/patches.h $dwmflexipatch_path/patches.h
-sudo cp $patches_url/config.h $dwmflexipatch_path/config.h
+sudo cp $patches_location/patches.h $dwmflexipatch_path/patches.h
+sudo cp $patches_location/config.h $dwmflexipatch_path/config.h
 
 # Add dwm session to xdm
 echo "exec dwm" > ~/.xsession
@@ -21,5 +23,3 @@ echo "exec dwm" > ~/.xinitrc #Fallback incase .xsession doesn't work
 # Start Compiling
 cd $dwmflexipatch_path
 sudo make clean install
-
-./set_max_volume.sh
